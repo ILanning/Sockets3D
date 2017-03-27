5a4783ef09e07db90021abdde4bf5e7497f75817
Ian Lanning <ianclanning@gmail.com>
2017-03-27 14:49:53 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+﻿var express = require("express");
+var bp = require("body-parser");
+var path = require("path");
+
+var port = 8000;
+var app = express();
+app.use(express.static(path.join(__dirname, "./client")));
+app.use(express.static(path.join(__dirname, "./node_modules")));
+app.use(bp.json());
+
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res) {
+    res.render("index");
+})
+
+var server = app.listen(port, function () { console.log("Listening on " + port) });
+
+var io = require("socket.io").listen(server);
+
+io.sockets.on("connection", function (socket) {
+    console.log("Sockets!");
+    console.log(socket.id);
+    socket.on("button_clicked", function (data) {
+        console.log('Someone clicked a button!  Reason: ' + data);
+        socket.emit('server_response', data );
+    })
+});
